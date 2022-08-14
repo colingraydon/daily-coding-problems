a32a88127ef12e7670527cfce99447fb83b5a1cc
Colin Graydon <colingraydon@gmail.com>
2022-08-14 13:28:30 -0400

    solved 339
    used window sliding

---
+++ dailycodingproblem339.py
@@ -0,0 +1,27 @@
+#Given an array of numbers and a number k, determine if there are three entries in the array which add up to the specified number k.
+
+#This is trivial in O(n^3), but this solution should solve it in O(n^2)
+
+def find_sum(l, sum):
+
+    length = len(l)
+    l.sort()
+    sum_exists = False
+
+    for i in range(0, length - 2):
+
+        j = i+1
+        k = length
+
+        while ( j < k ):
+
+            if (l[i] + l[j] + l[k] == sum):
+                sum_exists = True
+                print("Yes, the 3 elements are", l[i], l[j], l[k])
+            elif (l[i] + l[j] + l[k] < sum):
+                j += 1
+            else:
+                k -= 1
+    
+    if sum_exists is False:
+        print("No such entries exist")
